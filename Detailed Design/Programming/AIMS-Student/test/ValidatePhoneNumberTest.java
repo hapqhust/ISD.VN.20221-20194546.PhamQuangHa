5e31c8c215256f29832edd4cab1482fd7cc22bae
hapqhust <ha.pq194546@sis.hust.edu.vn>
2022-12-11 20:19:46 +0700

    lab08

---
+++ Detailed Design/Programming/AIMS-Student/test/ValidatePhoneNumberTest.java
@@ -0,0 +1,33 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import controller.PlaceOrderController;
+
+class ValidatePhoneNumberTest {
+
+	private PlaceOrderController placeOrderController;
+	@BeforeEach
+	void setUp() throws Exception {
+		placeOrderController = new PlaceOrderController();
+	}
+	@ParameterizedTest
+	@CsvSource({
+		"0123456789,true",
+		"01234,false",
+		"abc123,false",
+		"1234567890,false",
+	})
+
+	public void test(String phone, boolean expected) {
+		//when
+		boolean isValid = placeOrderController.validatePhoneNumber(phone);
+		
+		//then
+		assertEquals(expected, isValid);
+	}
+}
